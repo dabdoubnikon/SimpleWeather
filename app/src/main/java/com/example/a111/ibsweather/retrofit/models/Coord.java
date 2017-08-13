fa9de059b5ce1d44215e3e1f03893d5d06abe3cb
dabdoubnikon <applenikon89@gmail.com>
2017-08-13 16:55:11 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/a111/ibsweather/retrofit/models/Coord.java
@@ -0,0 +1,29 @@
+package com.example.a111.ibsweather.retrofit.models;
+
+
+public class Coord {
+    private String lon;
+
+    private String lat;
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPojo [lon = " + lon + ", lat = " + lat + "]";
+    }
+}
